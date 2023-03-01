cb77aabcebe2659db9493adb2037b2babf27d0e3
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2023-03-01 15:43:27 -0500

    remove files committd by accident

--- target/debug/build/libc-04eeb3f24713c17e/build_script_build-04eeb3f24713c17e.d
+++
@@ -1,5 +0,0 @@
-/home/luciano/Documents/github/ipcalc6/target/debug/build/libc-04eeb3f24713c17e/build_script_build-04eeb3f24713c17e: /home/luciano/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.121/build.rs
-
-/home/luciano/Documents/github/ipcalc6/target/debug/build/libc-04eeb3f24713c17e/build_script_build-04eeb3f24713c17e.d: /home/luciano/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.121/build.rs
-
-/home/luciano/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.121/build.rs:
--- target/debug/deps/atty-6eef746a03ff11c6.d
+++
@@ -1,7 +0,0 @@
-/home/luciano/Documents/github/ipcalc6/target/debug/deps/atty-6eef746a03ff11c6.rmeta: /home/luciano/.cargo/registry/src/github.com-1ecc6299db9ec823/atty-0.2.14/src/lib.rs
-
-/home/luciano/Documents/github/ipcalc6/target/debug/deps/libatty-6eef746a03ff11c6.rlib: /home/luciano/.cargo/registry/src/github.com-1ecc6299db9ec823/atty-0.2.14/src/lib.rs
-
-/home/luciano/Documents/github/ipcalc6/target/debug/deps/atty-6eef746a03ff11c6.d: /home/luciano/.cargo/registry/src/github.com-1ecc6299db9ec823/atty-0.2.14/src/lib.rs
-
-/home/luciano/.cargo/registry/src/github.com-1ecc6299db9ec823/atty-0.2.14/src/lib.rs:
